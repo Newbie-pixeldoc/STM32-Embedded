4bafbe5a126b8eda1577aba560756f95d844fa30
Newbie-pixeldoc <anaskham909654@gmail.com>
2025-08-27 23:43:02 +0530

    Bit banding

---
+++ codes/MISL2_Bit_banding.c
@@ -0,0 +1,27 @@
+#include<stdint.h>
+
+#define ALIAS_BASE   0x22000000U
+
+#define BITBAND_BASE 0x20000000U
+
+int main(void)
+{
+	uint8_t *ptr = (uint8_t*)0x20000200;
+
+	*ptr = 0xff;
+
+	//normal method
+	//clearing 7th bit position
+	*ptr &= ~( 1 << 7);
+
+	//reset to 0xff
+	*ptr = 0xff;
+
+	//bit band method
+	uint8_t *alias_addr = (uint8_t*) (ALIAS_BASE+(32 * (0x20000200-BITBAND_BASE))+ 7 * 4);
+
+	//clearing 7th bit of address 0x20000200
+	*alias_addr = 0;
+
+	for(;;);
+}
